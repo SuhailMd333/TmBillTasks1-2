33008be580cfbdc5eb7d0a8524aa8ec2d2261dfb
Suhail Shaikh <suhailshk333@gmail.com>
2025-05-12 08:19:54 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [react(),tailwindcss()],
+  preview: {
+    allowedHosts: [
+      'tmbilltasks1-2-1.onrender.com', // Your Render host
+      'localhost' // Keep for local testing
+    ],
+    host: '0.0.0.0', // Required for Render
+   
+  }
 })
